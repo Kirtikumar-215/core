83917f0a9cc1437947c4be967954d7de82393904
Kirtikumar-215 <kirtikumarprajapati01@gmail.com>
2026-03-14 11:21:13 +0530

    update project files

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -2,7 +2,8 @@ import os
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 
-load_dotenv()
+env_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env")
+load_dotenv(dotenv_path=env_path)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
